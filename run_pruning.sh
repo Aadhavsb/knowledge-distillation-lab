ec4e8f793809888f699bc2d419dd9627d226c99d
Aadhavsb <bharadwajaadhav@gmail.com>
2025-09-30 23:09:36 -0400

    Request V100 GPU to avoid CUDA compatibility issues with P100

--- run_pruning.sh
+++ run_pruning.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=pruning
 #SBATCH -p gpu
-#SBATCH -c 8
-#SBATCH --gres=gpu:1
-#SBATCH --mem=16GB
-#SBATCH -t 05:00:00
+#SBATCH -c 24
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem=32GB
+#SBATCH -t 24:00:00
 #SBATCH --output=pruning_%j.log
 
 module load Python/3.11.3-GCCcore-12.3.0
